e2a6a329e7bbe4b90e77e10902bd26d65562a8f0
Relakye <RHydrix@proton.me>
2024-12-27 00:47:16 -0500

    ugh

--- script.js
+++ script.js
@@ -1,57 +1,13 @@
-/*function darkon(){
-  var switchObjOn = document.getElementById('on');
-  var switchObjOff = document.getElementById('off');
-  var element = document.body;
-  var sidebar = document.getElementById('sidebar');
-  var contact = document.getElementsByClassName('contact');
-  var generic = document.getElementsByClassName('gen-objBar-border');
-  switchObjOn.style.display = 'none';
-  switchObjOff.style.display = 'inherit';
-  //element.classList.toggle("itemColor-darkMode");
-  element.classList.toggle("itemColor-darkMode");
-  sidebar.classList.toggle("sidebar-darkMode");
-  for(i=0; i<contact.length; i++) {
-    contact[i].classList.toggle("contact-darkMode")
-  }
-  for(j=0; j<generic.length; j++) {
-    generic[j].classList.toggle("progBar-darkMode")
-  }
-}
-
-function lighton(){
-  var switchObjOn = document.getElementById('on')
-  var switchObjOff = document.getElementById('off')
-  var element = document.body;
-  var sidebar = document.getElementById('sidebar');
-  var contact = document.getElementsByClassName('contact');
-  var generic = document.getElementsByClassName('gen-objBar-border');
-  switchObjOn.style.display = 'inherit';
-  switchObjOff.style.display = 'none';
-  element.classList.toggle("itemColor-darkMode");
-  sidebar.classList.toggle("sidebar-darkMode");
-  for(i=0; i<contact.length; i++) {
-    contact[i].classList.toggle("contact-darkMode")
-  }
-  for(j=0; j<generic.length; j++) {
-    generic[j].classList.toggle("progBar-darkMode")
-  }
-}
-
-function listLoop(array) {
-  for(i=0; i<array.length; i++) {
-    array[i].classList.toggle("contact-darkMode")
-  }
-}
-*/
-function onClick(element) {
+/*function onClick(element) {
   document.getElementById("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
   var captionText = document.getElementById("caption");
   captionText.innerHTML = element.alt;
-}
+}*/
 
 // Change style of navbar on scroll
 window.onscroll = function() {myFunction()};
+
 function myFunction() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
