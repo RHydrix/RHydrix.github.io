89078e8f2e68fc6b7c8918a092502b150dfc3bd2
Relakye <RHydrix@proton.me>
2025-03-02 22:58:28 -0600

    Overlay
    top overlay Max width, lower images fix, changed link (nyi),

--- script.js
+++ script.js
@@ -9,20 +9,20 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "objBar" + " objCard" + " objAnimate-top" + " itemColour-white";
-    } else {
-        navbar.className = navbar.className.replace(" objCard objAnimate-top itemColour-white", "");
-    }
+  var navbar = document.getElementById("myNavbar");
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      navbar.classList.add("objCard", "objAnimate-top", "itemColour-white");
+  } else {
+      navbar.classList.remove("objCard", "objAnimate-top", "itemColour-white");
+  }
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
-    var x = document.getElementById("nav");
-    if (x.classList.contains("objShow")) {
-        x.classList.remove("objShow");
-    } else {
-        x.classList.add("objShow");
-    }
+  var x = document.getElementById("navGate");
+  if (x.classList.contains("objShow")) {
+      x.classList.remove("objShow");
+  } else {
+      x.classList.add("objShow");
+  }
 }
