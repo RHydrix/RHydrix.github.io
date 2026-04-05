f3d2d9e8fb0479e54ba7f6d7abd6a9c86bc1a663
Relakye <RHydrix@proton.me>
2026-04-05 03:25:52 -0500

    site update Patch 4/5/2026

--- script.js
+++ script.js
@@ -6,19 +6,19 @@
 }*/
 
 // Change style of navbar on scroll
-window.addEventListener('scroll', myFunction);
+window.addEventListener("scroll", myFunction);
 
 function myFunction() {
-  var navbar = document.getElementById("myNavbar");
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      navbar.classList.add("objCard", "objAnimate-top", "itemColour-white");
-  } else {
-      navbar.classList.remove("objCard", "objAnimate-top", "itemColour-white");
-  }
+	var navbar = document.getElementById("myNavbar");
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		navbar.classList.add("objCard", "objAnimate-top", "itemColour-white");
+	} else {
+		navbar.classList.remove("objCard", "objAnimate-top", "itemColour-white");
+	}
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
+/*function toggleFunction() {
     var x = document.getElementById("navGate");
     var isShown = x.classList.toggle("objshow");
     var btn = document.getElementById("menuBtn");
@@ -27,9 +27,9 @@ function toggleFunction() {
      btn.setAttribute('aria-expanded', String(isShown));
      x.setAttribute('aria-hidden', String(!isShown));
     }
-}
+}*/
 
-document.addEventListener('DOMContentLoaded', function () {
-  var btn = document.getElementById('menuBtn');
-  if (btn) btn.addEventListener('click', toggleFunction);
-});
+/*document.addEventListener("DOMContentLoaded", function () {
+	var btn = document.getElementById("menuBtn");
+	if (btn) btn.addEventListener("click", toggleFunction);
+});*/
